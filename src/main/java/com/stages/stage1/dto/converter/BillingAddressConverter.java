632de4f3026e405f8f73339b74bc32c2e4df5937
keri_daniel <daniel.keri@moon42.com>
2019-07-12 10:06:22 +0200

    new akarmi

--- src/main/java/com/stages/stage1/dto/converter/BillingAddressConverter.java
+++ src/main/java/com/stages/stage1/dto/converter/BillingAddressConverter.java
@@ -37,5 +37,6 @@ public class BillingAddressConverter {
         response.setInvoiceNumber(billingAddressRepository.findAllById(ids).size());
 
         return response;
+        ///adsadasd
     }
 }
