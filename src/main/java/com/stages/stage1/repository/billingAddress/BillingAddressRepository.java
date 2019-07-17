cd2d40b607a0aa4e9b2d0e2460fe7e4bf173ed02
intern <kristof.berta@moon42.com>
2019-07-17 17:51:45 +0200

    added query variation of findAllByWebsiteUserId to BillingAddress endpoints and commented out the one with filter added some missing exception throwing

--- src/main/java/com/stages/stage1/repository/billingAddress/BillingAddressRepository.java
+++ src/main/java/com/stages/stage1/repository/billingAddress/BillingAddressRepository.java
@@ -2,8 +2,16 @@ package com.stages.stage1.repository.billingAddress;
 
 import com.stages.stage1.entity.BillingAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BillingAddressRepository extends JpaRepository<BillingAddress, UUID> {
+
+    @Query(value = "SELECT ba FROM BillingAddress ba " +
+            "WHERE " +
+            "ba.websiteUser.id = :id")
+    public List<BillingAddress> findAllByWebsiteUserId(@Param(value = "id") UUID id);
 }
